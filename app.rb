3d032179c890ee9922565ffd3da7da10d44d18af
Paul <pcherian10@gmail.com>
2018-06-22 14:49:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -13,7 +14,7 @@ class App < Sinatra::Base
 
   post '/checkout' do
     @session = params["item"]
-
+    binding.pry
     erb :item
   end
 
